b5f1ae9b047bdb9c44046c73f4e3fcd683516edd
Federico Vilensky <fedevilensky@gmail.com>
2024-08-10 12:27:01 -0300

    added interaction trigger to player object

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class player_movement : MonoBehaviour
@@ -8,11 +10,12 @@ public class player_movement : MonoBehaviour
     [SerializeField]
     [Range(1.0f, 10.0f)]
     private float speed = 5.0f;
+    private Collider2D interactionTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        interactionTrigger = GetComponentsInChildren<Collider2D>().FirstOrDefault(c => c.gameObject.name == "interaction");
     }
 
     // Update is called once per frame
@@ -25,6 +28,37 @@ public class player_movement : MonoBehaviour
             horizontal = Mathf.Round(Input.GetAxis("Horizontal"));
         }
         var move = new Vector3(horizontal, vertical, 0);
-        transform.position += move * Time.deltaTime * speed;
+
+        MovePosition(move);
+        RotateInteractionTrigger(move);
+    }
+
+    private void RotateInteractionTrigger(Vector3 move)
+    {
+        if (move.x != 0)
+        {
+            interactionTrigger.transform.rotation = Quaternion.Euler(0, 0, move.x > 0 ? 0 : 180);
+        }
+        else if (move.y != 0)
+        {
+            interactionTrigger.transform.rotation = Quaternion.Euler(0, 0, move.y > 0 ? 90 : 270);
+        }
+    }
+
+    private void MovePosition(Vector3 move)
+    {
+        transform.position += move * (speed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Callback to draw gizmos that are pickable and always drawn.
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        if (interactionTrigger != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(interactionTrigger.bounds.center, interactionTrigger.bounds.size);
+        }
     }
 }
